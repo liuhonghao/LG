595dd6a36847fd1ec96664cf088554daa2bd3c28
刘弘浩 <76436932+liuhonghao@users.noreply.github.com>
2021-05-01 20:40:44 +0800

    Add files via upload

---
+++ P5711闰年判断/P5711闰年判断/T.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	if ((n%400 == 0) || (n % 100 != 0 && n % 4 == 0))
+//	{
+//		cout << 1 << endl;
+//	}
+//	else
+//	{
+//		cout << 0 << endl;
+//	}
+//	return 0;
+//}
+
